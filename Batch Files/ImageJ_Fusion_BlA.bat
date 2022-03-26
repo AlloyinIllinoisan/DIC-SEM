18b5fb876dd1f616ffbffa506663bffa3f6b52b9
AlloyinIllinoisan <99607042+AlloyinIllinoisan@users.noreply.github.com>
2022-03-25 23:02:58 -0500

    Update and rename ImageJ_Fusion_BlA.bat to Batch Files/ImageJ_Fusion_BlA.bat

---
+++ Batch Files/ImageJ_Fusion_BlA.bat
@@ -0,0 +1,19 @@
+@echo off
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/MakeFuseBlA_r9.ijm"	
+TIMEOUT /T 20
