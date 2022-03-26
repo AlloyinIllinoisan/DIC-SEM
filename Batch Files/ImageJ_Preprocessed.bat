9f99fb0351f7e48b6f9d89e64b90054f2d74606c
AlloyinIllinoisan <99607042+AlloyinIllinoisan@users.noreply.github.com>
2022-03-25 22:59:55 -0500

    Rename ImageJ_Preprocessed.bat to Batch Files/ImageJ_Preprocessed.bat

---
+++ Batch Files/ImageJ_Preprocessed.bat
@@ -0,0 +1,19 @@
+@echo off
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r1.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r2.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r3.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r4.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r5.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r6.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r7.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r8.ijm"	
+TIMEOUT /T 20
+START "My Program" /D "C:\Program Files (x86)\Fiji.app" ImageJ-win64.exe -macro "D:/Scripts/RLB/Overnight Run/ImageJ_Macros/PreprocessedMacro_BlN_r9.ijm"	
+TIMEOUT /T 20
