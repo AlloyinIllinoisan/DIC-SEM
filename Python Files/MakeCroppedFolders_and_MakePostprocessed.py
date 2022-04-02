04220bc1f44b598a5ff5f597ce7d391507253c6e
AlloyinIllinoisan <99607042+AlloyinIllinoisan@users.noreply.github.com>
2022-04-02 13:18:45 -0500

    Update MakeCroppedFolders_and_MakePostprocessed.py

--- Python Files/MakeCroppedFolders_and_MakePostprocessed.py
+++ Python Files/MakeCroppedFolders_and_MakePostprocessed.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar  5 22:55:57 2022
-
-@author: repha
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Sat Oct 30 15:44:25 2021
 
-@author: Moose
+@author: Rephayah
 """
 
 import pandas as pd
@@ -107,7 +100,7 @@ for j in range(1,n+1):
     c0r03 = data[0][0:3]
 
     #Extract Data from y-Column of text file
-    col3extrctddata = column3[3:12]
+    col3extrctddata = column3[3:]
     a = col3extrctddata.values
     
     #Populate Dummy Matrix w/ Divided Numbers from Column 3
@@ -118,7 +111,7 @@ for j in range(1,n+1):
         b.append(float(c[0])/subsampling) #Append all values into list
 
     #Extract Data from x-Column of text file
-    col2extrcteddata = column2[3:12]
+    col2extrcteddata = column2[3:]
     d = col2extrcteddata.values
 
     #Populate Dummy Matrix w/ Stripped Values from Column 2
@@ -143,44 +136,26 @@ for j in range(1,n+1):
 
     #Convert Lists to Arrays for x-Column
     cc2r03 = np.array(c2r03)
-    ll = np.array(l)
+    aa = np.array(l)
 
     #Separate Filler Rows & Divided Rows
-    na0 = cc2r03[0]
-    na1 = cc2r03[1]
-    na2 = cc2r03[2]
-    na3 = ll[0]
-    na4 = ll[1]
-    na5 = ll[2]
-    na6 = ll[3]
-    na7 = ll[4]
-    na8 = ll[5]
-    na9 = ll[6]
-    na10 = ll[7]
-    na11 = ll[8]
+
+    naarray = np.append(cc2r03,aa)
+
 
     #Append Filler Rows & Divided Rows, convert to Series
-    newcol2 = pd.Series([na0, na1, na2, na3, na4, na5, na6, na7, na8, na9, na10,na11])
+    newcol2 = pd.Series(naarray)
 
     #Convert Lists to Arrays for y-Column
     cc3r03 = np.array(c3r03)
-    ww = np.array(w)
+    bb = np.array(w)
     #Separate Filler Rows & Divided Rows
-    nb0 = cc3r03[0]
-    nb1 = cc3r03[1]
-    nb2 = cc3r03[2]
-    nb3 = ww[0]
-    nb4 = ww[1]
-    nb5 = ww[2]
-    nb6 = ww[3]
-    nb7 = ww[4]
-    nb8 = ww[5]
-    nb9 = ww[6]
-    nb10 = ww[7]
-    nb11 = ww[8]
+
+    nbarray = np.append(cc3r03,bb)
+
     
     #Append Filler Rows & Divided Rows, convert to Series
-    newcol3 = pd.Series([nb0, nb1,nb2, nb3,nb4,nb5,nb6,nb7,nb8,nb9,nb10,nb11])
+    newcol3 = pd.Series(nbarray)
     
     #Concatenate All Columns & Convert to Data Frame
     newtextfile = pd.DataFrame([column0,column1,newcol2,newcol3,column4,column5,column6,column7,column8,column9])
@@ -189,7 +164,7 @@ for j in range(1,n+1):
     #Export Data Frame as Text File
     # finalproduct.to_csv(path+'/Postprocessed.txt', header=None, index=None, sep=' ', mode='a')
 
-    col0extrcteddata = column0[3:12]
+    col0extrcteddata = column0[3:]
     q = col0extrcteddata.values
     
     cc0r03 = np.array(c0r03)
@@ -224,77 +199,38 @@ for j in range(1,n+1):
     rr = np.array(r) #BlN
     kk = np.array(k) #BlA
     
-    np0 = cc0r03[0]
-    np1 = cc0r03[1]
-    np2 = cc0r03[2]
-    np3 = pp[0]
-    np4 = pp[1]
-    np5 = pp[2]
-    np6 = pp[3]
-    np7 = pp[4]
-    np8 = pp[5]
-    np9 = pp[6]
-    np10 = pp[7]
-    np11 = pp[8]
+
+    nparray = np.append(cc0r03,pp)
+
     
-    newcol0_LRt = pd.Series([np0,np1,np2,np3,np4,np5,np6,np7,np8,np9,np10,np11])
+    newcol0_LRt = pd.Series(nparray)
     #Concatenate All Columns & Convert to Data Frame
     newtextfile_LRt = pd.DataFrame([newcol0_LRt,column1,newcol2,newcol3,column4,column5,column6,column7,column8,column9])
     finalproduct_LRt = newtextfile_LRt.T #Transpose Data Frame
     
-    nz0 = cc0r03[0]
-    nz1 = cc0r03[1]
-    nz2 = cc0r03[2]
-    nz3 = zz[0]
-    nz4 = zz[1]
-    nz5 = zz[2]
-    nz6 = zz[3]
-    nz7 = zz[4]
-    nz8 = zz[5]
-    nz9 = zz[6]
-    nz10 = zz[7]
-    nz11 = zz[8]
+
+    nzarray = np.append(cc0r03,zz)
+
     
-    newcol0_GrX = pd.Series([nz0, nz1,nz2, nz3,nz4,nz5,nz6,nz7,nz8,nz9,nz10,nz11])
+    newcol0_GrX = pd.Series(nzarray)
     
     #Concatenate All Columns & Convert to Data Frame
     newtextfile_GrX = pd.DataFrame([newcol0_GrX,column1,newcol2,newcol3,column4,column5,column6,column7,column8,column9])
     finalproduct_GrX = newtextfile_GrX.T #Transpose Data Frame
     
-    nr0 = cc0r03[0]
-    nr1 = cc0r03[1]
-    nr2 = cc0r03[2]
-    nr3 = rr[0]
-    nr4 = rr[1]
-    nr5 = rr[2]
-    nr6 = rr[3]
-    nr7 = rr[4]
-    nr8 = rr[5]
-    nr9 = rr[6]
-    nr10 = rr[7]
-    nr11 = rr[8]
+
+    nrarray = np.append(cc0r03,rr)
+
     
-    newcol0_BlN = pd.Series([nr0, nr1,nr2, nr3,nr4,nr5,nr6,nr7,nr8,nr9,nr10,nr11])
+    newcol0_BlN = pd.Series(nrarray)
 
     #Concatenate All Columns & Convert to Data Frame
     newtextfile_BlN = pd.DataFrame([newcol0_BlN,column1,newcol2,newcol3,column4,column5,column6,column7,column8,column9])
     finalproduct_BlN = newtextfile_BlN.T #Transpose Data Frame
-   
-    nk0 = cc0r03[0]
-    nk1 = cc0r03[1]
-    nk2 = cc0r03[2]
-    nk3 = kk[0]
-    nk4 = kk[1]
-    nk5 = kk[2]
-    nk6 = kk[3]
-    nk7 = kk[4]
-    nk8 = kk[5]
-    nk9 = kk[6]
-    nk10 = kk[7]
-    nk11 = kk[8]
 
-    newcol0_BlA = pd.Series([nk0, nk1,nk2, nk3,nk4,nk5,nk6,nk7,nk8,nk9,nk10,nk11])
-
+    nkarray = np.append(cc0r03,kk)
+    newcol0_BlA = pd.Series(nkarray)
+ 
     #Concatenate All Columns & Convert to Data Frame
     newtextfile_BlA = pd.DataFrame([newcol0_BlA,column1,newcol2,newcol3,column4,column5,column6,column7,column8,column9])
     finalproduct_BlA = newtextfile_BlA.T #Transpose Data Frame
