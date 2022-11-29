1e978f40c9d610361ff8611edf17d0eaff8aceab
Rephayah Black <99607042+AlloyinIllinoisan@users.noreply.github.com>
2022-11-29 12:11:28 -0600

    Update Align_Images.py

--- Automation_at_its_finest/Good to Go/Align_Images.py
+++ Automation_at_its_finest/Good to Go/Align_Images.py
@@ -71,6 +71,7 @@ GridCols = lines[9][11:-1] #Number of columns in the grid
 GridRows = lines[10][11:-1] #Number of rows in the grid
 TileOverlap = lines[11][14:-1] #Percentage of overlap between tiles
 PythonScriptsDirectory = lines[12][26:-1] #Python Scripts Directory (Good to Go)
+PythonScriptsDirectory = PythonScriptsDirectory.replace("\\","/")
 xCorrelDirectory = lines[13][17:] #Path to xCorrel
 directory = MainDirectory
 path = ImageJMacrosDirectory
