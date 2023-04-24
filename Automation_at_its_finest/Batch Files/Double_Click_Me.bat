68eb4adca48be6f339ee36bdeaa3c4527afe001c
Rephayah Black <99607042+AlloyinIllinoisan@users.noreply.github.com>
2023-04-24 15:43:27 -0500

    Update Double_Click_Me.bat
    Signed-off-by: Rephayah Black <99607042+AlloyinIllinoisan@users.noreply.github.com>

--- Automation_at_its_finest/Batch Files/Double_Click_Me.bat
+++ Automation_at_its_finest/Batch Files/Double_Click_Me.bat
@@ -6,7 +6,8 @@ call Generate_Tiffs_from_Raw_Data_amended_Batch.bat
 call ImageJ_Preprocessed.bat
 call MakeCroppedFoldersandMakePostprocessed_batch.bat
 call ImageJ_Fusion_BlN.bat
-call ImageJ_Fusion_GrX.bat
+call ImageJ_Fusion_GrX_3pxGB.bat
+call ImageJ_Fusion_GrX_NoBlur.bat
 call ImageJ_Fusion_BlA.bat
 call ImageJ_Fusion_Tws.bat
 call ImageJ_Fusion_LRt.bat
