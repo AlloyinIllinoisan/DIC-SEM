a4a3b6ddd622ce07370896cfff5483c26fbfc3ab
Rephayah Black <99607042+AlloyinIllinoisan@users.noreply.github.com>
2023-04-28 12:23:44 -0500

    Update XCorrel_Batch_File_Generator.py
    Signed-off-by: Rephayah Black <99607042+AlloyinIllinoisan@users.noreply.github.com>

--- Automation_at_its_finest/Good to Go/XCorrel_Batch_File_Generator.py
+++ Automation_at_its_finest/Good to Go/XCorrel_Batch_File_Generator.py
@@ -56,7 +56,7 @@ f.write("echo start calcul\n")
 f.write("pause\n")
 for i in range(1,b+1):
     for j in range(1,n+1):
-        f.write("start /wait XCorrel_V9.20b.exe %s\\IMAFile_p%s_%s.ima\n" % (newpath,i,j))
+        f.write("start /wait XCorrel_V9.20c.exe %s\\IMAFile_p%s_%s.ima\n" % (newpath,i,j))
     print('file done')
 f.write("call XCorrel_Finished_%s.bat" % (ProjectName))
 f.close()
