108da10690904bf8740da9b7c967cdb0af91f27d
Rephayah Black <99607042+AlloyinIllinoisan@users.noreply.github.com>
2023-12-11 12:10:16 -0600

    Add files via upload
    Signed-off-by: Rephayah Black <99607042+AlloyinIllinoisan@users.noreply.github.com>

---
+++ Automation_at_its_finest/Good to Go/Executor_ForCollaborators.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 16:21:17 2022
+
+@author: Rephayah
+"""
+
+# exec("MakeCroppedFolders_and_MakePostprocessed_Batch.py")
+exec(open("Make_Image_Alignment_Batch_File.py").read())
+exec(open("Align_Images_Batch.py").read())
+exec(open("XCorrel_Batch_File_Generator_ForCollaborators.py").read())
+# exec(open("Create_AlignedImagesDirectory_Batch.py").read())
+exec(open("Generate_Tiffs_from_Raw_Data_amended_Batch.py").read())
+# exec(open("AddStepNumber_Batch.py").read())
+exec(open("MakeCroppedFolders_and_MakePostprocessed_Batch.py").read())
+exec(open("NewSorter_Batch.py").read())
+#Seeing if I can circumvent certain batch files
+exec(open("Create_ImageJ_MakeFuse_Batch_Files.py").read())
+exec(open("Create_AlignedImagesDirectory.py").read())
+exec(open("Align_Images.py").read())
+exec(open("Create_Preprocessed_ImageJ_Macros.py").read())
+exec(open("Create_ImageJ_Preprocessed_Batch_Files.py").read())
+exec(open("Create_Fuse_ImageJ_Macros_BlN.py").read())
+exec(open("Create_Fuse_and_Blur_ImageJ_Macros_GrX.py").read())
+exec(open("Create_Fuse_and_Blur_ImageJ_Macros_BlA.py").read())
+exec(open("Create_Fuse_and_Blur_ImageJ_Macros_LRt.py").read())
+exec(open("Create_Fuse_ImageJ_Macros_Tws.py").read())
+exec(open("AddStepNumber.py").read())
+exec(open("Finished_Batch_ForCollaborators.py").read())
+exec(open("XCorrel_Finished_Batch_ForCollaborators.py").read())
+exec(open("Make_XCorrel_Batch_Finished_Batch_File.py").read())
---
+++ Automation_at_its_finest/Good to Go/XCorrel_Finished_Batch_ForCollaborators.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import tkinter as tk
+import csv
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  6 16:53:39 2022
+
+@author: Rephayah
+"""
+
+import time
+import csv
+import pandas as pd
+import numpy as np
+import re
+import smtplib
+import time
+import ssl
+import yagmail
+
+#     #open folder 
+# from tkinter import filedialog
+# root = tk.Tk()
+# root.withdraw()
+# root.attributes("-topmost", True)
+# directory = filedialog.askdirectory()
+# root.destroy()
+
+tic = time.time()
+ScriptName = 'Make Cropped Folders and Postprocessed Text Files Batch File'
+
+with open('Pilot.txt') as f:
+    lines = f.readlines()
+    
+MainDirectory = lines[0][16:-1] #Main Folder
+MainDirectory = MainDirectory.replace("\\","/")
+n = lines[1][-2] #Number of Steps
+ImageJMacrosDirectory = lines[2][25:-1] #Folder Containing the ImageJ Macros
+ImageJMacrosDirectory = ImageJMacrosDirectory.replace("\\","/")
+BatchFileMacrosDirectory = lines[3][23:-1] #Folder Containing the Batch Files
+BatchFileMacrosDirectory = BatchFileMacrosDirectory.replace("\\","/")
+PathtoFiji = lines[4][14:-1] #Path to ImageJ Fiji
+SendToEmail = lines[5][12:-1] #Your email
+WorkStationEmail = lines[6][23:-1] #Marshawn's Email
+Password = lines[7][10:-1] #Password for Marshawn to get to his email
+ProjectName = lines[8][14:-1] #Name of the current project
+GridCols = lines[9][11:-1] #Number of columns in the grid
+GridRows = lines[10][11:-1] #Number of rows in the grid
+TileOverlap = lines[11][14:-1] #Percentage of overlap between tiles
+PythonScriptsDirectory = lines[12][26:-1] #Python Scripts Directory (Good to Go)
+PythonScriptsDirectory = PythonScriptsDirectory.replace("\\","/")
+xCorrelDirectory = lines[13][17:-1] #Path to xCorrel
+PythonDirectory = lines[14][16:-1] #Path to Python
+# Folder Path
+path = MainDirectory
+
+L1 = '''@echo off
+setlocal'''
+n = int(n)
+
+lemon = []
+
+
+L2 = '''call {}
+cd "{}"
+python XCorrel_Finished_ForCollaborators.py'''.format(PythonDirectory,PythonScriptsDirectory)
+lemon.append(L2)
+    
+npL1 = np.array(L1)
+nplemon = np.array(lemon)
+Jeepers = pd.Series([npL1])
+
+Idaho = pd.DataFrame(Jeepers)    
+Oregon = pd.DataFrame(nplemon)
+California = pd.concat([Idaho,Oregon])
+
+California.to_csv(BatchFileMacrosDirectory+'/XCorrel_Finished.bat', index=False, header=False, sep='\t', quoting=csv.QUOTE_NONE, escapechar = '\t')
+
+# yag = yagmail.SMTP('{}'.format(WorkStationEmail), '{}'.format(Password))
+
+# contents = ['{} Completed'.format(ScriptName)]
+
+# yag.send('{}'.format(SendToEmail), '{}'.format(ProjectName), contents)
+    
+toc = time.time()
+
+print(toc-tic, 'seconds elapsed')
+
